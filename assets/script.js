94d0c551cd2a0e2d1bb397671f79876ec2c72021
Stephanie Mathis <stephaniedmathis@gmail.com>
2022-01-29 23:12:51 -0500

    updated js file

--- assets/script.js
+++ assets/script.js
@@ -175,8 +175,9 @@ questions = [
 		answer: "prototype chain"
 	},
 
+])
 
-]
+
 //WHEN all questions are answered or the timer reaches 0, THEN the game is over
 
 //WHEN the game is over, THEN I can save my initials and my score
